04576dcd1a88f2913c566db391a4d42cd9470728
Hira <hira.duggal@gmail.com>
2023-06-29 14:37:32 +0530

    commit initial changes to first branch

---
+++ src/SportsAPIs/Infra/DataAccess/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Data.SqlClient;
+using System.Data;
+using Microsoft.Extensions.Configuration;
+
+namespace Sports.Infra.DataAccess
+{
+    public interface ISportsDatabaseContext
+    {
+        public IDbConnection CreateConnection();
+    }
+    public class SportsDatabaseContext : ISportsDatabaseContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+        public SportsDatabaseContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("SportsConnection");
+        }
+        public IDbConnection CreateConnection()
+            => new SqlConnection(_connectionString);
+    }
+}
