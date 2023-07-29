ef88fd689f3084d323466122abdb04428abe4d87
HiraDuggal <138106611+HiraDuggal@users.noreply.github.com>
2023-07-29 19:33:27 +0530

    setup sports db (#2)

---
+++ src/SportsDb/dbo/Tables/PlayerProfile.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[PlayerProfile] (
+    [PlayerProfileId]    INT IDENTITY (1, 1) NOT NULL,
+    [RighthandBatter]    BIT NULL,
+    [LefthandBatter]     BIT NULL,
+    [RightarmBowler]     BIT NULL,
+    [LeftarmBowler]      BIT NULL,
+    [WicketKeeper]       BIT NULL,
+    [OpeningBatter]      BIT NULL,
+    [MiddlerOrderBatter] BIT NULL,
+    [LowerOrderBatter]   BIT NULL
+);
+
